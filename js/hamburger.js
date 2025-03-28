590f9ee85240be9a55869a6b495d126bfe81cd09
Joshua <joshfouch777@gmail.com>
2025-03-28 17:01:24 -0400

    moving website

--- js/hamburger.js
+++
@@ -1,9 +0,0 @@
-function navToggle() {
-    var items = document.getElementById("navItems");
-    if (!items.classList.contains("responsive")) {
-        items.classList.add("responsive");
-    }
-    else {
-        items.classList.remove("responsive");
-    }
-}
